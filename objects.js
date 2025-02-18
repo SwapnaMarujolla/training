88e296c40ad187e6f96b9877a568e951d92d6bc6
Poojitha <poojithaglok@gmail.com>
2025-02-18 17:35:10 +0530

    add an object file

---
+++ objects.js
@@ -0,0 +1,15 @@
+//example for objects
+ let person =  {
+    name:'Poojitha',
+    age: 30
+}
+ console.log(person);
+
+
+ // Dot notation
+//  person.name = 'John';
+//console.log(person.name);
+
+//Bracket notation
+// let selection = 'name';
+// person['selection'] = 'Mary';
