ccde6d0e6d5edee2ecdd1c01cb458f39e78be61f
Poojitha <poojithaglok@gmail.com>
2025-02-18 12:25:59 +0530

    add constants file

---
+++ constants.js
@@ -0,0 +1,11 @@
+// let intrestRate ;
+// console.log('intrestRate',intrestRate);
+//     intrestRate = 1;
+// console.log('intrestRate',intrestRate);
+
+//array examples
+let selectedColor =['red','blue'];
+    // 
+    selectedColor[2]= 2;
+    console.log(selectedColor);
+
