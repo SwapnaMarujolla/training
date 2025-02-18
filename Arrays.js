7544be9418154d2aee026d056e52d0024538d550
Poojitha <poojithaglok@gmail.com>
2025-02-18 17:52:17 +0530

    Add an array file

--- Arrays.js
+++ Arrays.js
@@ -7,12 +7,12 @@ let selectedColor =['red','blue'];// an array literal
 //In js we can add different datatypes in the same list of objects and the size is dynamic
 
 
-let selectedColor =['red','blue'];
-    selectedColor[2]= '4';// we can add 2 strings and a number
-    console.log(selectedColor);
+// let selectedColor =['red','blue'];
+//     selectedColor[2]= '4';// we can add 2 strings and a number
+//     console.log(selectedColor);
 
 
-//FINDING PROPERTIES OF AN ARRAY  
-let selectedColor =['red','blue'];
-selectedColor[2]= '4';// we can add 2 strings and a number
-console.log(selectedColor.length);//this sho number of properties or items in an array
+// //FINDING PROPERTIES OF AN ARRAY  
+// let selectedColor =['red','blue'];
+// selectedColor[2]= '4';// we can add 2 strings and a number
+// console.log(selectedColor.length);//this sho number of properties or items in an array
