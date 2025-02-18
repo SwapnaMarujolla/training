606f3b0523c0336eb172a252eb5b4fb04c8f6aca
Poojitha <poojithaglok@gmail.com>
2025-02-18 17:49:18 +0530

    Add an Array file

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@
 }
  console.log(person);
 
-
+// objects have key value pairs and bunch of properties that can be accessed using a dot notation
  // Dot notation
 //  person.name = 'John';
 //console.log(person.name);
