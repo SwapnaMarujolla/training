a8c5a11c37c891f4973c5fe605a1d0b60c4721e1
Poojitha <poojithaglok@gmail.com>
2025-02-18 19:32:26 +0530

    Add a function file

---
+++ functions.js
@@ -0,0 +1,12 @@
+ function greet(name){
+     //name is a parameter
+     console.log('Hello'+ name);
+ }
+greet('John'); //jhon is an argument
+
+//function is a building block in js performs set of tasks and calculates the values
+//functions can have input and each input changes on how they behave
+// function greet(name,lastName){       
+// console.log('Hello' + name ,lastName);
+// }
+// greet('John','Smith'); 
