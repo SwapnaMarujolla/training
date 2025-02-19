5f664af653469dcd2fabea55131a452a6ea34cc6
Poojitha <poojithaglok@gmail.com>
2025-02-19 13:38:31 +0530

    Add a conditional file

---
+++ Conditional.js
@@ -0,0 +1,9 @@
+// TERNARY OR CONDITIONAL OPERATORS
+
+let point = 110;
+let type = point > 100 ? 'gold' : 'silver';
+console.log(type);
+
+// if a customer has more than 100 points ,
+// they are a gold customer otherwise
+// they are the silver customer
