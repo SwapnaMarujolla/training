8d464dd125044032df9223ed7681087f5edbc23a
Poojitha <poojithaglok@gmail.com>
2025-02-19 11:51:27 +0530

    Add an Assignment operator file

---
+++ Assignment.js
@@ -0,0 +1,21 @@
+//ASSIGNMENT OPERATORS
+// assigns values to the variables
+
+
+let x = 5;  // x gets the value 
+
+// let x = 5;
+// x += 5;  // becomes x = x + 5 (x becomes 15)
+
+// let x = 10;
+// x -= 3;  // this becomes x = x - 3 (x becomes 7)
+
+// let x = 4;
+// x *= 2;  // This becomes x = x * 2 (x becomes 8)
+
+// let x = 10;
+// x /= 2;  // This becomes x = x / 2 (x becomes 5)
+
+// let x = 10;
+// x %= 3;  //This becomes x = x % 3 (x becomes 1)
+
