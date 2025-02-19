fe732dcf4f48708244ef3b10e1d2fc75499ea21e
Poojitha <poojithaglok@gmail.com>
2025-02-19 23:01:45 +0530

    Add a  modified file

--- constants.js
+++ constants.js
@@ -1,10 +1,17 @@
-let intrestRate ;
-console.log('intrestRate',intrestRate);
- intrestRate = 1;
-console.log('intrestRate',intrestRate);
+//  let intrestRate ;
+//   intrestRate = 1;
+// console.log('intrestRate',intrestRate);
 
-//array examples
-// let selectedColor =['red','blue'];
-//     selectedColor[2]= 2;
-//     console.log(selectedColor);
+
+
+let radius = 5;
+const pi = 3.14;
+area = pi * radius * radius;
+ // radius = 6;the new value of the radius will be considered as 6
+// pi = 5;
+// console.log(area);
+// here we can change the value of the variable but the connstant i.e, the value of pi remains the same.
+// in const once we initialize a value that cannot be changed.
+// if we tey to change a constant value it will give an error Assignment to a constant value.
+// if instead of const if we declare the pi value as let it will accept and gives some particular value
 
