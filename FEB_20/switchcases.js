af0375e6db7a25b48d91a667bbf8908ea680fb46
Poojitha <poojithaglok@gmail.com>
2025-02-20 15:39:12 +0530

    Add a switch case file

---
+++ FEB_20/switchcases.js
@@ -0,0 +1,16 @@
+let role;
+
+switch (role){
+    case 'guest':
+    console.log('guest user');
+    break;
+
+    case 'moderator user':
+    console.log('moderator user');
+    break;
+
+    default:
+    console.log('Unknown user');
+    break;
+
+}
