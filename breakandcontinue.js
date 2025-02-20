5abece92c7155fa26d544c1336e125d9897ba039
Poojitha <poojithaglok@gmail.com>
2025-02-20 17:23:49 +0530

    Add a break and continue file

---
+++ breakandcontinue.js
@@ -0,0 +1,18 @@
+// EXAMPLE FOR BREAK STATEMENT
+let i = 0;
+while(i < 10){
+    if (i==5) break;
+        console.log(i);
+        i++;
+}
+ 
+// EXAMPLE FOR CONTINUE STATEMENT
+ i = 0;
+ while (i <=10){
+    if (i % 2 == 0){
+        i++;
+    continue;
+ }
+ console.log(i);
+ i++;
+}
