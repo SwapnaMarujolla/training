3fb7609154cebc83e0a02c4e5b7c8b922ce0835e
Poojitha <poojithaglok@gmail.com>
2025-02-20 16:27:53 +0530

    Add a whileloop file

---
+++ whileloop.js
@@ -0,0 +1,17 @@
+ // EXAMPLE 1
+  let i = 0;
+  while(i<=10){
+      console.log(i);
+    i++;
+ }
+ //EXAMPLE 2
+  i = 0;
+ while(i<10){
+     if(i == 5) break;// whenever we use break it comes out of the loop if the condition satisfies
+    console.log(i);
+
+    i++;
+  }
+
+
+
