aed800034b1b0648a351380991ed75c5743016c6
Poojitha <poojithaglok@gmail.com>
2025-02-20 16:53:00 +0530

    Add a dowhile loop file

---
+++ Dowhileloop.js
@@ -0,0 +1,12 @@
+//EXAMPLE OF DO WHILE LOOP
+let i = 9;
+do{
+    if(i % 2 !==0);
+    console.log(i);
+    i++;
+}while(i<=5);
+
+// here do while loop executes atleast once
+// it firstly checks for the condition in the do loop and executes it
+// at last it checks for the while conditiion
+// thats why we get 9 as a result because it checks for an odd number in the do loop
