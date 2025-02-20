977b52a0a816c7ee52ad1b5d54c6a781bb4f929f
Poojitha <poojithaglok@gmail.com>
2025-02-20 16:05:09 +0530

    Add a for loop to a file

---
+++ forloop.js
@@ -0,0 +1,13 @@
+//EXAMPLE 1
+for (i=0; i<5;i++){
+    console.log("Hello world",i)// i is the index value
+}
+
+//this loop runs until the condition in the loop becomes false
+
+//EXAMPLE 2
+//PRINTING ODD NUMBERS BETWEEN 0 TO 5
+for (i = 0;i<=5;i++){
+    if(i % 2 !==0)
+        console.log(i)
+}
