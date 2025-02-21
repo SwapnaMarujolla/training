e9b0f7ed0df2139d336a46e91090433d401ea8e5
Poojitha <poojithaglok@gmail.com>
2025-02-21 16:41:02 +0530

    Add a truthy file

---
+++ FEB_21/exercise6.js
@@ -0,0 +1,16 @@
+
+const array = [1, 2, 3];  
+
+function countTruthy(array) {
+    let count = 0;
+    for(let value of array) {  
+        if(value)              
+            count++;
+    }
+    return count;
+}
+
+console.log(countTruthy(array));
+
+
+
