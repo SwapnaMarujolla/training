598eadc12bab5c24ca337511f703927f50730e63
Poojitha <poojithaglok@gmail.com>
2025-02-21 12:52:22 +0530

    Add a max of two numbers file

--- FEB_21/exercise1.js
+++ FEB_21/exercise1.js
@@ -1,9 +1,15 @@
 // Max of two numbers
-let number = max(1,2)
+let number = max(5,2)
 console.log(number);
 // 
 function max(a,b){
+   
     if(a>b){
         return a;
     }else return b;
 }
+// return (a>b) ? a : b
+// we can also do this using a conditional operator
+
+
+
