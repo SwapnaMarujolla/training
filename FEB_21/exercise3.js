4df530c9271a0fbd06ba975f20707df04e6c3745
Poojitha <poojithaglok@gmail.com>
2025-02-21 15:27:36 +0530

    Add a Fizzbuzz file

---
+++ FEB_21/exercise3.js
@@ -0,0 +1,20 @@
+// FISSBUZZ
+ 
+const output = fizzBuzz(15);
+console.log(output);
+
+function fizzBuzz(input){ 
+    
+    if ((input % 3 === 0) && (input % 5 === 0))
+    return "fizzBuzz";
+
+
+    if(input % 3 === 0)
+        return "Fizz";
+
+    if(input % 5 === 0)
+        return "Buzz";
+    
+
+    return input;
+}
