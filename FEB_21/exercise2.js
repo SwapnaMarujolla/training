32fb2580c340c9249b58061fa1f6cfcf0f36c305
Poojitha <poojithaglok@gmail.com>
2025-02-21 13:13:32 +0530

    Add a landscape function file

---
+++ FEB_21/exercise2.js
@@ -0,0 +1,6 @@
+console.log(isLandscape(900,500));
+
+function isLandscape(width,height){
+    return(width>height);
+}
+//  instead of writing a conditional statement we can  simply return the value using a comparision opertor
