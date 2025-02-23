a8011da6dc5e662c66d2b8aa640e9ef0f013df9b
Poojitha <poojithaglok@gmail.com>
2025-02-23 20:28:33 +0530

    add an enumerating file

--- FEB_22/enumerating.js
+++ FEB_22/enumerating.js
@@ -8,15 +8,10 @@ const circle = {
    
    
    for (let key in circle)
-
    console.log(key, circle [key]);
-   
    
    for (let key of Object.keys(circle))// we use this to print all the properties in an object
    
    console.log(key);
-   
-   
     for (let entry of Object.entries(circle))
-       
    console.log(entry);
