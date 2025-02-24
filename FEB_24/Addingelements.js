4035828162ca19cf5a0eeb36b618e9b65f6e4a3a
Poojitha <poojithaglok@gmail.com>
2025-02-24 13:27:33 +0530

    Add an adding element file

---
+++ FEB_24/Addingelements.js
@@ -0,0 +1,13 @@
+const numbers = [3,4];
+
+// Adding numbers at the end
+numbers.push(5, 6);
+
+// pushes elements to the right Adding numbers in the begining
+numbers.unshift(7, 8);
+
+// by adding a splicing method we can add or remove elements at any position
+numbers.splice(2,0,'a','b');
+
+console.log(numbers);
+
