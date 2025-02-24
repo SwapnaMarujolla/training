e413d2055d4d617bfe3cb3907839da1280831c20
Poojitha <poojithaglok@gmail.com>
2025-02-24 16:37:40 +0530

    Add a finding array file

--- FEB_24/Findingelements.js
+++ FEB_24/Findingelements.js
@@ -2,6 +2,6 @@ const numbers = [1,2,3,1,4];
 
 console.log(numbers.indexOf('1'));//if the element is not in an array then it returns -1
 
-console.log(numbers.lastIndexOf(1));
+console.log(numbers.lastIndexOf(1));// it finds the number or element from last in an array returning the index position o an array
 
 console.log(numbers.includes(1));// checks whether the element is present in a given array
