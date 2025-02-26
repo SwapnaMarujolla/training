88e6443d0a464b0f27b1160dcb988431bf9d9dc5
Poojitha <poojithaglok@gmail.com>
2025-02-26 16:51:40 +0530

    add includes.js file

---
+++ FEB_26/exercise2a.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3, 4];
+console.log(numbers.includes(10));
+
+function includes (array, searchElement) {
+for (let element of array)
+if (element === searchElement)
+    return true;
+return false;
+}
