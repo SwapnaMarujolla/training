e87e9bc89bbea370db04891860f510693a58ebb4
Poojitha <poojithaglok@gmail.com>
2025-03-04 09:32:27 +0530

    Add a file

---
+++ MAR_03/join.js
@@ -0,0 +1,8 @@
+const nums = ['Poojitha GL','Swapna M','Chaithanya BM','Bhoomika BM','Kartheek M','Prathik M'];
+// const abcd = 'this is my newcode';
+const def = nums.join('and')
+// const abc = nums.split(',');
+// console.log(abc);
+console.log(def);
+
+
