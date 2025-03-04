2cdff9fb1b9cf6c6a28acd7596bcb7640edb60fb
Poojitha <poojithaglok@gmail.com>
2025-03-04 09:30:46 +0530

    Add a file

---
+++ MAR_03/switch.js
@@ -0,0 +1,24 @@
+const week = 'tuesday';
+
+switch (week.toUpperCase()){
+    case 'MONDAY':
+        console.log('its a week day');
+        break;
+    case 'TUESDAY':
+        console.log('its a week day');
+        break;
+    case 'WEDNESDAY':
+        console.log('its a week day');
+        break;
+    case 'THURSDAY':
+        console.log('its a week day');
+        break;
+    case 'FRIDAY':
+        console.log('its a week day');
+        break;
+    default : 
+         console.log('its a weekend');
+
+};
+
+
