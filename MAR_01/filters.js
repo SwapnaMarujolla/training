5144cd0dc465140901a1532016f23f1da374c642
Poojitha <poojithaglok@gmail.com>
2025-03-04 09:35:26 +0530

    Add a file

---
+++ MAR_01/filters.js
@@ -0,0 +1,16 @@
+const numbers = [1,2,-3,8,-5,6,-10]
+const positiveNumbers = numbers.filter(function(value){
+    return value >0;
+});
+console.log(positiveNumbers);
+
+// const number = [1,2,3,-5,-8,9,-11]
+// for (let i = 0; i < number.length; i++) {
+//     if (number[i] > 0) {
+//         console.log(number[i]);
+//     }
+// }
+    
+    
+
+
