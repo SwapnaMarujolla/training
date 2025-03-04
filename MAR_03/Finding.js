fd859015c93c51fc0f0be41921dc011ae7506a8b
Poojitha <poojithaglok@gmail.com>
2025-03-04 09:32:01 +0530

    Add  a file

---
+++ MAR_03/Finding.js
@@ -0,0 +1,7 @@
+const numbers = [1,100];
+console.log(numbers.indexOf(100));//this gives what is in that index
+
+//  console.log(numbers.includes(6));// this gives whether the array includes the element or not i.e, true or false
+
+// console.log(numbers.lastIndexOf(4))//gives the number of the index from last
+
