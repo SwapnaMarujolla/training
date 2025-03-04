77949779470d1860fc8301c0bb9906d06e9c3203
Poojitha <poojithaglok@gmail.com>
2025-03-04 09:33:46 +0530

    add a file

---
+++ MAR_03/adding.js
@@ -0,0 +1,14 @@
+// using push
+const alphabets = ['a','b','c','d']
+alphabets.push('d','e');
+console.log(alphabets);
+
+// using unshift
+const numbers = [3,4,5,6,7]
+numbers.unshift(1,2);
+console.log(numbers);
+
+// splicing
+const evenNumbers = [2,4,8,10]
+evenNumbers.splice(2,2,6);
+console.log(evenNumbers);
